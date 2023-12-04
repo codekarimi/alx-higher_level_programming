e26a4d597732a51baf034ffbc4a342cb5977e55b
zeus254 <ckarimi676@gmail.com>
2023-12-03 22:18:48 -0800

    Python Network

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,9 +9,6 @@ Created on Mon Aug 24 07:02:53 2020
 from requests import post, codes
 from sys import argv
 
-
-
-
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
     if len(argv) > 1:
